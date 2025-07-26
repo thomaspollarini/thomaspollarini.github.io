570b6294b00e883d2d8c8da0e31d02332541e3cf
Thomas <thomassantospallarini@gmail.com>
2025-07-26 18:22:57 -0300

    feat: add toogle theme and main & skill content

---
+++ assets/js/scripts.js
@@ -0,0 +1,16 @@
+function toggleTheme() {
+  const currentTheme = document.documentElement.getAttribute('data-theme');
+  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+  document.documentElement.setAttribute('data-theme', newTheme);
+}
+
+function changeToggleIcon(toggleButton) {
+  toggleButton.classList.toggle('bi-moon-stars');
+  toggleButton.classList.toggle('bi-sun');
+}
+
+const themeToggleButton = document.querySelector('#toggleTheme');
+themeToggleButton.addEventListener('click', () => {
+  toggleTheme();
+  changeToggleIcon(themeToggleButton);
+});
